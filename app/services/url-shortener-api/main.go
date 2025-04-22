208f6297b4b9b139ba1653e86da1f5a7476d505b
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-04-22 20:55:10 +0200

    Added configuration setup - app\services\url-shortener-api

--- app/services/url-shortener-api/main.go
+++ app/services/url-shortener-api/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
+	"time"
 
 	"github.com/MinaMamdouh2/URL-Shortener/foundation/logger"
+	"github.com/ardanlabs/conf/v3"
 	"go.uber.org/zap"
 )
 
@@ -35,6 +38,36 @@ func main() {
 func run(log *zap.SugaredLogger) error {
 	// GOMAXPROCS
 	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "BUILD", build)
+
+	// -------------------------------------------------------------------------
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			ReadTimeout     time.Duration `conf:"default:5s,env:READ_TIMEOUT"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+		}
+	}{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "Mina Mamdouh",
+		},
+	}
+
+	const prefix = "URL_SHORTENER"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
 
 	// -------------------------------------------------------------------------
 	// Shuting down protocol
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/MinaMamdouh2/URL-Shortener
 
 go 1.24.1
 
-require go.uber.org/zap v1.27.0
+require (
+	github.com/ardanlabs/conf/v3 v3.7.2
+	go.uber.org/zap v1.27.0
+)
 
 require go.uber.org/multierr v1.10.0 // indirect
