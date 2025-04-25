788a10f53c36cdf46a4fa7e4bdc9ad2df0eb1be8
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-04-25 19:52:48 +0300

    Refactored v1 package to add RouteAdder interface - business\web\v1

--- business/web/v1/v1.go
+++ business/web/v1/v1.go
@@ -19,21 +19,25 @@ type APIMuxConfig struct {
 	Log *zap.SugaredLogger
 }
 
+// RouteAdder defines behavior that sets the routes to bind for an instance
+// of the service.
+type RouteAdder interface {
+	Add(router *gin.Engine, cfg APIMuxConfig)
+}
+
 // WebAPI constructs a http.Handler with all application routes bound.
 // In type systems and functions, we don't use interface type as a return type unless it is the error interface or maybe
 // you need the empty interface because you are not using generics.
 // Here we are violating this rule by returning the http.Handler but why we are doing that? for one reason he wants us
 // to have this conversation with us.
-// We shouldn't decouple anything for the callee ideally this should be what is returned here "*httptreemux.ContextMux"
+// We shouldn't decouple anything for the callee ideally this should be what is returned here "*gin.Engine"
 // We should return the concrete type and let the caller decide if it needs to abstract that or not.
-func APIMux(cfg APIMuxConfig) http.Handler {
+// Now we are passing the config and pass it some concrete value that knows how to Add "routeAdder", this creates a level
+// of abstraction here and "routeAdder" is going to call Add passing in the mux we wanna bind the route to and the config.
+func APIMux(cfg APIMuxConfig, routeAdder RouteAdder) http.Handler {
 	router := gin.New()
 
-	router.GET("/hack", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"Status": "Ok",
-		})
-	})
+	routeAdder.Add(router, cfg)
 
 	return router
 }
