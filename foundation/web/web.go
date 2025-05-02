577f469a33ed7c63243f0390c50ab61be9cbfd26
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-05-02 22:10:27 +0300

    Added application level middlewares, also added wrapping application level middlewares and specific middlewares in  Handle function - foundation/web

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -21,16 +21,21 @@ type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) e
 type App struct {
 	*gin.Engine
 	shutdown chan os.Signal
+	// We are gonna add a slice of middleware functions
+	mw []Middleware
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
 // The new function constructs a new App and because this New function is in the web package we are calling it "NewApp"
 // because the type is called App if it was called web we would have "New" instead of "NewApp" this is the idiom.
-func NewApp(shutdown chan os.Signal) *App {
+// We added a variadic signature, we are gonna pass in a collection of middleware functions and since that becomes
+// a slice we can do that also these are the middlewares that we wanna route every single time.
+func NewApp(shutdown chan os.Signal, mw ...Middleware) *App {
 	engine := gin.New()
 	return &App{
 		Engine:   engine,
 		shutdown: shutdown,
+		mw:       mw,
 	}
 }
 
@@ -39,16 +44,30 @@ func NewApp(shutdown chan os.Signal) *App {
 // We now have overridden the mux's Handle method because now that the method exists that method overrides the promotion
 // and it is saying Bill I don't want your mux handle signature I want the App Handle signature this was the case for
 // 'httptreemux" but we are using gin so this is not the current case.
-func (a *App) Handle(method string, path string, handler Handler) {
+// We added a variadic parameter mw here, this is for middlewares used for a particular route like authentication.
+func (a *App) Handle(method string, path string, handler Handler, mw ...Middleware) {
 	// Now we are going to build the onion.
+	// Wrapping specific middlewares first
+	handler = wrapMiddleware(mw, handler)
+	// Wrapping application level middlewares so they are called first.
+	handler = wrapMiddleware(a.mw, handler)
 	// We know at the end of the day, we need a function that takes an Http response writer, and a pointer to http request
 	// in order to use the contextMux handle function
 	h := func(c *gin.Context) {
-		err := handler(c.Request.Context(), c.Writer, c.Request)
-		if err != nil {
+		// Add any logic here
+		// Now we have set the handler "H" at the center of the onion.
+		// We know that in the foundation layer we are not allowed to log, and the logic we need to inject needs to be
+		// in the business layer because business layer code is allowed to log.
+		// Somebody can easily say now I am going to import the logger or I am going to pass the logger in here but
+		// you can't because the moment you do that this code is no longer reusable for anybody unless they are using
+		// the same logger. So we need a way for injecting middleware but a middleware that can exist anywhere in the
+		// application so the code we are injecting is following the proper rules and guidelines and idioms.
+		if err := handler(c.Request.Context(), c.Writer, c.Request); err != nil {
 			// Handle error: could enhance this to log, metrics, etc.
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
+		// Add any logic here
 	}
 
 	// We can create all the abstraction in the world but at the end of the day what is implementing the mux is the
