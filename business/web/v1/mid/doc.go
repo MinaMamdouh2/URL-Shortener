d564d71ef2cb6134ad6b2b4e45627cd190bc7a5b
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-05-03 14:00:58 +0300

    Added mid containment package in business

---
+++ business/web/v1/mid/doc.go
@@ -0,0 +1,6 @@
+package mid
+
+// This a containment package. BUt why? we said that we should avoid that.
+// We can get away with them if the surface array of the import is going to be tiny
+// and it is tiny in this package so we won't flip the pyramid here.
+// We have very small functions here that it doesn't make sense to create a whole packages from middleware functions.
