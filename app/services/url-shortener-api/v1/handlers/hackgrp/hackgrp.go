952799d633966b6484e6fe7d2b80552852dfca73
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-05-09 18:40:59 +0300

    Added a mock for how to return a trusted error in hackgrp handlers - app\services\url-shortener-api\v1\handlers\hackgrp

--- app/services/url-shortener-api/v1/handlers/hackgrp/hackgrp.go
+++ app/services/url-shortener-api/v1/handlers/hackgrp/hackgrp.go
@@ -3,12 +3,21 @@ package hackgrp
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"math/rand"
 	"net/http"
+
+	"github.com/MinaMamdouh2/URL-Shortener/business/web/v1/response"
 )
 
+// if it is OK but we don't want the handler dealing with error handling, we would rather push the error handling back
+// into the business layer if we could or somewhere outside the handler for consistency
 // Hack handles the /hack route using Gin context.
 func Hack(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
+	if n := rand.Intn(100) % 2; n == 0 {
+		return response.NewError(errors.New("TRUSTED ERROR"), http.StatusBadRequest)
+	}
 	status := struct {
 		Status string
 	}{
