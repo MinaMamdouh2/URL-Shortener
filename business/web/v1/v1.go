6722ff9f73360f1f96e2e4380509d2d98c3359aa
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-05-09 21:37:20 +0300

    Added metrics middleware in APIMux middleware chain - business\web\v1

--- business/web/v1/v1.go
+++ business/web/v1/v1.go
@@ -43,7 +43,7 @@ type RouteAdder interface {
 // abstraction.
 // Notice we are not creating an abstraction to an interface we are creating an abstraction to the concrete type.
 func APIMux(cfg APIMuxConfig, routeAdder RouteAdder) *web.App {
-	app := web.NewApp(cfg.Shutdown, mid.Logger(cfg.Log), mid.Errors(cfg.Log), mid.Panics())
+	app := web.NewApp(cfg.Shutdown, mid.Logger(cfg.Log), mid.Errors(cfg.Log), mid.Metrics(), mid.Panics())
 
 	routeAdder.Add(app, cfg)
 
