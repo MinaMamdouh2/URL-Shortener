de50646fc028ed1c971d761e2059446afb42a143
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-05-13 20:22:19 +0300

    Added auth support in hackgrp handlers - app\services\url-shortener-api\v1\handlers

--- app/services/url-shortener-api/v1/handlers/hackgrp/route.go
+++ app/services/url-shortener-api/v1/handlers/hackgrp/route.go
@@ -3,12 +3,22 @@ package hackgrp
 import (
 	"net/http"
 
+	"github.com/MinaMamdouh2/URL-Shortener/business/web/v1/auth"
+	"github.com/MinaMamdouh2/URL-Shortener/business/web/v1/mid"
 	"github.com/MinaMamdouh2/URL-Shortener/foundation/web"
 )
+
+// Config contains all the mandatory systems required by handlers.
+type Config struct {
+	Auth *auth.Auth
+}
 
 // Routes adds specific routes for this group using Gin.
 // Each group of handlers can define routes related to its domain and bind them to the router that is passed in.
 // web.App is everything a gin engine is because of the embedding
-func Routes(app *web.App) {
+func Routes(app *web.App, cfg Config) {
+	authen := mid.Authenticate(cfg.Auth)
+	ruleAdmin := mid.Authorize(cfg.Auth, auth.RuleAdminOnly)
 	app.Handle(http.MethodGet, "/hack", Hack)
+	app.Handle(http.MethodGet, "/hackauth", Hack, authen, ruleAdmin)
 }
