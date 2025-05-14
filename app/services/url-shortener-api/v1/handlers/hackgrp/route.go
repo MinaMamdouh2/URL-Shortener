4e14751462950717de2f525a1affa752e70a4ae4
MinaMamdouh2 <40996073+MinaMamdouh2@users.noreply.github.com>
2025-05-14 17:01:21 +0300

    Added v1 version in hackgrp routes - app\services\url-shortener-api\v1\handlers

--- app/services/url-shortener-api/v1/handlers/hackgrp/route.go
+++ app/services/url-shortener-api/v1/handlers/hackgrp/route.go
@@ -17,8 +17,9 @@ type Config struct {
 // Each group of handlers can define routes related to its domain and bind them to the router that is passed in.
 // web.App is everything a gin engine is because of the embedding
 func Routes(app *web.App, cfg Config) {
+	const version = "v1"
 	authen := mid.Authenticate(cfg.Auth)
 	ruleAdmin := mid.Authorize(cfg.Auth, auth.RuleAdminOnly)
-	app.Handle(http.MethodGet, "/hack", Hack)
-	app.Handle(http.MethodGet, "/hackauth", Hack, authen, ruleAdmin)
+	app.Handle(http.MethodGet, version, "/hack", Hack)
+	app.Handle(http.MethodGet, version, "/hackauth", Hack, authen, ruleAdmin)
 }
